940ba4796230891fe14ae8962b32852e688eb2db
GloriaBimbi <gloria.bimbi1@libero.it>
2024-01-25 17:44:53 +0100

    fix readme snack 4

--- snack 4/README.md
+++ snack 4/README.md
@@ -10,3 +10,14 @@ Usa l'array allegato alla traccia per completare i seguenti punti:
 # BONUS
 
 Stampare in pagina oltre che in console!
+
+## SVOLGIMENTO:
+
+- STEP 1: creo un array che conterrà gli oggetti con le chiavi nome e falli subiti
+- STEP 2: recupero l'elemento html di cui ho bisogno
+- STEP 3: creo un ciclo che scorre l'array delle squadre
+- STEP 4: con il destructing estraggo le variabili nome, punteggio e falloi subiti
+- STEP 5: genero casualmente il numero di punti fatti e dei falli subiti in un range scelto da me che va da 0 a 10
+- STEP 6: stampo in pagina i risultati della partita
+- STEP 7: nel nuovo array di squadre inserisco degli oggetti che contengono solo il nome e i falli subiti
+- STEP 8: stampo in console il nuovo array di squdre
--- snack 4/js/main.js
+++ snack 4/js/main.js
@@ -39,17 +39,18 @@ const teams = [
 // creo un array che conterrà gli oggetti con le chiavi nome e falli subiti
 const newTeamsArray = [];
 
-//recupero l'elemento html di cui ho bisogno
+// recupero l'elemento html di cui ho bisogno
 const listCountainer = document.getElementById("list-container");
 
 // creo un ciclo che scorre l'array delle squadre
 teams.forEach((team) => {
-  //con il destructing estraggo le variabili nome, punteggio e falloi subiti
+  // con il destructing estraggo le variabili nome, punteggio e falloi subiti
   let { name, score, foul } = team;
   // genero casualmente il numero di punti fatti e dei falli subiti in un range scelto da me che va da 0 a 10
   score = generateRandomNumber(0, 10);
   foul = generateRandomNumber(0, 10);
   console.log(`Punteggio: ${score}; Falli subiti: ${foul}`);
+  // stampo in pagina i risultati della partita
   listCountainer.innerHTML += `
   <li>
     La squadra ${name} ha totalizzato ${score} punti e ha subito ${foul} falli
