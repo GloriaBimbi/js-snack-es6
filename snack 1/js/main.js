c619076dc27f3048594dfba6eb29caf24b1225f3
GloriaBimbi <gloria.bimbi1@libero.it>
2024-01-25 10:12:31 +0100

    fix snack 1

--- snack 1/js/main.js
+++ snack 1/js/main.js
@@ -14,13 +14,11 @@ const vips = [
 
 // modifico l'array sostituendo alla stringa del nome di ogni vip, un oggetto che contiene il numero del tavolo, il suo nome e il suo posto
 const vipsDetails = vips.map((vip, index) => {
-  const vipObject = {
+  return {
     tableName: "Tavolo Vip",
     vipName: vip,
     spotNumber: index + 1,
   };
-  console.log(vips[0]);
-  return vipObject;
 });
 // stampo in console l'array di oggetti per i segnaposto
 console.log(vipsDetails);
