22bc8c5039cb729267ea5947f2d7a50180344833
GloriaBimbi <gloria.bimbi1@libero.it>
2024-01-26 16:52:19 +0100

    feat snack 1 in gruppi

---
+++ snack 5/js/main.js
@@ -0,0 +1,35 @@
+// // SNACK 1 IN GRUPPI: dato un array di macchine, chiedi all'utete con un prompt quante persone vogliono viaggiare trova la prima macchina con almeno quella capacità
+
+// let cars = [
+//   {
+//     color: "purple",
+//     type: "minivan",
+//     capacity: 7,
+//   },
+//   {
+//     color: "red",
+//     type: "station wagon",
+//     capacity: 5,
+//   },
+//   {
+//     color: "yellow",
+//     type: "utilitaria",
+//     capacity: 2,
+//   },
+//   {
+//     color: "orange",
+//     type: "berlina",
+//     capacity: 4,
+//   },
+// ];
+// const passengersNumb = parseInt(prompt("Con quante persone vuoi viaggiare"));
+
+// let rightCar = "";
+// cars.forEach((car) => {
+//   if (car.capacity >= passengersNumb) {
+//     rightCar = car;
+//   }
+// });
+// console.log(
+//   `La macchina che ti interessa è la ${rightCar.color} ${rightCar.type} con capacità ${rightCar.capac}`
+// );
