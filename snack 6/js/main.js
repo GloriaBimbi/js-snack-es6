1ca564849cebee374653146d881e7ad1d19660ed
GloriaBimbi <gloria.bimbi1@libero.it>
2024-01-26 17:35:28 +0100

    feat snack 2 in gruppo

---
+++ snack 6/js/main.js
@@ -0,0 +1,26 @@
+// SNACK 2 IN GRUPPI: abbiamo un array di oggetti (frutta) con titolo e prezzo.
+// generiamo un numero randomico che si riferisce ai frutti da mettere in macedonia.
+// Sommiamo il numero frutti uscito dell'array e diciamo quanto costa la macedonia in totale.
+
+const products = [
+  { title: "Mela", price: 2.3 },
+  { title: "Banana", price: 1.5 },
+  { title: "Mango", price: 2 },
+  { title: "Pesca", price: 1.8 },
+  { title: "Pera", price: 1.5 },
+  { title: "Arancia", price: 2.2 },
+];
+
+const number = generateRandomNumber(1, 6);
+console.log("Numero randomico: " + number);
+
+let price = 0;
+let ingredienti = products[0].title;
+
+for (i = 1; i < number; i++) {
+  price += products[i].price;
+  ingredienti = ingredienti + ", " + products[i].title;
+}
+console.log(
+  `Il prezzo totale della macedonia di ${ingredienti} è di: €${price}`
+);
